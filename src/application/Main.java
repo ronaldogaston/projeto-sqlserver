7868ac26ad66e73b551c20de7c7408a253e27dd5
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-12-24 13:47:26 -0300

    Design MainView.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,28 +4,25 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/telas/MainView.fxml"));
-			Scene scene = new Scene(root, 400, 400);
-			Parent parent = loader.load();
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Scene mainScene = new Scene(parent);
-			primaryStage.setScene(scene);
+			ScrollPane scrollPane = loader.load();
+
+			scrollPane.setFitToHeight(true); // Serve para ajustar o MenuBar (Barra de tarefas da parte de cima)
+			scrollPane.setFitToWidth(true); // Serve para ajustar o MenuBar (Barra de tarefas da parte de cima)
+
+			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Aplicação JavaFX");
 			primaryStage.show();
-			primaryStage.show();
 		} catch (IOException e) {
-			e.printStackTrace();
 			e.printStackTrace();
 		}
 	}
