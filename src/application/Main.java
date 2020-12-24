514c04189906ba128189d5d5568389f8d9b91211
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-12-24 13:26:01 -0300

    Implementando a MainView.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,35 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/telas/MainView.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			Parent parent = loader.load();
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene mainScene = new Scene(parent);
 			primaryStage.setScene(scene);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Aplicação JavaFX");
 			primaryStage.show();
-		} catch(Exception e) {
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
